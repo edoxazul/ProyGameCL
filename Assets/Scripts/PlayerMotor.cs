0759fdbbd28b1bdd364d532382fd326034cacbdd
Eduardo <edualvsald@hotmail.com>
2020-04-16 22:33:06 -0400

    pruebas de movimiento de personaje y camara

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller; 
+    private float speed = 5.0f;
+    private Vector3 moveVector;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController> (); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity*Time.deltaTime; 
+        }
+        moveVector = Vector3.zero;
+        // valor en X izq- der
+        moveVector.x = Input.GetAxisRaw("Horizontal")*speed;
+        // valor en Y arriba - abajo
+        moveVector.y = verticalVelocity; 
+        // valor en Z atras - adelante
+        moveVector.z = speed; 
+
+        controller.Move(moveVector* Time.deltaTime); 
+
+    }
+}
