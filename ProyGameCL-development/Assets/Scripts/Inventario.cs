ee14ca3a436ec644db09ca571152ea4368c1a39d
Vedran Tomicic <vedran.tomicic@alumnos.ucn.cl>
2020-05-08 16:03:11 -0400

    Cambio de escena
    Desde el menú al juego en sí

---
+++ ProyGameCL-development/Assets/Scripts/Inventario.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System;
+using UnityEngine;
+
+public class Inventario{
+    private int gemas;
+    private int monedas;
+    public Inventario()
+    {
+        try
+        {
+            TextReader l= new StreamReader("InstanciaInventario.txt");
+        }
+        catch
+        {
+            TextWriter linea1 = new StreamWriter("InstanciaInventario.txt");
+            linea1.WriteLine("0"); //gemas
+            linea1.Close();
+            StreamWriter linea2 = File.AppendText("InstanciaInventario.txt");
+            linea2.WriteLine("0"); //monedas
+            linea2.Close();
+        }
+        TextReader linea = new StreamReader("InstanciaInventario.txt");
+        gemas = Int32.Parse(linea.ReadLine());
+        monedas = Int32.Parse(linea.ReadLine());
+        linea.Close();
+    }
+
+    public void AumentarGemas(int cantidad)
+    {
+        gemas = gemas + cantidad;
+        TextWriter linea1 = new StreamWriter("InstanciaInventario.txt");
+        linea1.WriteLine(gemas);
+        linea1.Close();
+        StreamWriter linea2 = File.AppendText("InstanciaInventario.txt");
+        linea2.WriteLine(monedas); //monedas
+        linea2.Close();
+    }
+    public void AumentarMonedas(int cantidad)
+    {
+        monedas = monedas + cantidad;
+        TextWriter linea1 = new StreamWriter("InstanciaInventario.txt");
+        linea1.WriteLine(gemas);
+        linea1.Close();
+        StreamWriter linea2 = File.AppendText("InstanciaInventario.txt");
+        linea2.WriteLine(monedas); //monedas
+        linea2.Close();
+    }
+    public void DisminuirMonedas(int cantidad)
+    {
+        monedas = monedas - cantidad;
+        TextWriter linea1 = new StreamWriter("InstanciaInventario.txt");
+        linea1.WriteLine(gemas);
+        linea1.Close();
+        StreamWriter linea2 = File.AppendText("InstanciaInventario.txt");
+        linea2.WriteLine(monedas); //monedas
+        linea2.Close();
+    }
+    public void DisminuirGemas(int cantidad)
+    {
+        gemas = gemas - cantidad;
+        TextWriter linea1 = new StreamWriter("InstanciaInventario.txt");
+        linea1.WriteLine(gemas);
+        linea1.Close();
+        StreamWriter linea2 = File.AppendText("InstanciaInventario.txt");
+        linea2.WriteLine(monedas); //monedas
+        linea2.Close();
+    }
+    public void MostrarGemas()
+    {
+        Debug.Log("la cantidad de gemas son: "+gemas);
+    }
+}
