f20bfe0dd76781980be23af9be9256a8dd53bf24
Eduardo <edualvsald@hotmail.com>
2020-05-08 18:46:24 -0400

    Correccion de errores

---
+++ Assets/Scripts/ClickPlay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class ClickPlay : MonoBehaviour
+{
+  public void BtnScene(string name)
+    {
+        SceneManager.LoadScene(name); 
+    }
+}
