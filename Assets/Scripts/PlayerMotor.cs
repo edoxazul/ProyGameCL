40379443adc1cc98a88abbac6cf85f8d53072d7a
Eduardo <edualvsald@hotmail.com>
2020-05-09 08:31:33 -0400

    add: se agrega el spartan warrior , para evitar errores

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Diagnostics;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
