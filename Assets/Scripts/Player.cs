84cddb7c76f33710f884b0801d71633717324fde
Matias <matias.ortiz@alumnos.ucn.cl>
2020-09-01 01:11:11 -0400

    Auto stash before merge of "development" and "origin/development"

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim;
+    public int number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        setAnimationTo(number);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void setAnimationTo(int number)
+    {
+        anim.SetInteger("character", number);
+    }
+}
